89830749c2391a1fd93608b5d178e871de75d450
Leslie Thompson <lcrthompson@gmail.com>
2019-04-03 16:10:25 -0700

    todo.js added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import TodoForm from "./components/TodoComponents/TodoForm";
+import Todo from "./components/TodoComponents/Todo";
+import TodoList from "./components/TodoComponents/TodoList";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
